5c87400e1dfa4e9291b1f79507ac66ad5300e568
sozibhossain <sozibhossain69@gmail.com>
2025-04-13 18:16:18 +0600

    update

--- src/app/(website)/dashboard/_components/dashboard-sidebar-item.tsx
+++ src/app/(website)/dashboard/_components/dashboard-sidebar-item.tsx
@@ -8,13 +8,11 @@ import React from 'react';
 const DashboardSidebarItem = ({item}: {item : SidebarContentType}) => {
     const pathName = usePathname();
     const isActive = pathName === item?.href;
-    console.log(isActive)
-    console.log({item})
     return (
         <div>
-            <Link className={cn(`flex items-center justify-start gap-4 p-4 text-lg font-medium leading-[120%] mb-4  ${isActive ? "bg-primary text-white rounded-[4px] border-r-[4px] border-primary-50" : "bg-transparent text-black" }`)} href={item?.href}>{item?.icon} {item?.name}</Link>
+            <Link className={cn(`flex items-center justify-start gap-4 p-4 text-lg font-medium leading-[120%] mb-4  ${isActive ? "bg-primary text-white rounded-[4px] border-r-[4px] border-primary-50" : "bg-transparent text-black" }`)} href={item?.href}>  {item?.name}</Link>
         </div>
     );
 };
-
+  
 export default DashboardSidebarItem;
