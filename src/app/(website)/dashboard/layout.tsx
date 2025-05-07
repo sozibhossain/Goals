03bb95388e9f61d0aaa3fdd3d60204f9f9c0cba4
sozibhossain <sozibhossain69@gmail.com>
2025-05-07 11:26:13 +0600

    update

--- src/app/(website)/dashboard/layout.tsx
+++ src/app/(website)/dashboard/layout.tsx
@@ -1,14 +1,20 @@
+// app/(admin)/dashboard/layout.tsx
+
+
 import React, { ReactNode } from "react";
 import DashboardSidebar from "./_components/dashboard-sidebar";
+import AuthWrapper from "@/components/AuthWrapper"; // adjust path if needed
 
 const AdminDashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-white w-full ">
-      <div className="w-full h-full md:flex lg:flex justify-start items-start ">
-        <DashboardSidebar />
-        <div className="w-full px-2 lg:px-7  pt-6">{children}</div>
+    <AuthWrapper>
+      <div className="bg-white w-full">
+        <div className="w-full h-full md:flex lg:flex justify-start items-start">
+          <DashboardSidebar />
+          <div className="w-full px-2 lg:px-7 pt-6">{children}</div>
+        </div>
       </div>
-    </div>
+    </AuthWrapper>
   );
 };
 
